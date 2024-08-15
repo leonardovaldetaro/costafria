9175d85c3649163ee2cf70a783662d47d399b456
Leonardo Valdetaro <leonardovaldetaro@gmail.com>
2024-08-15 12:59:31 +0100

    first commit

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+export default function AppRouter () {
+    return (
+        <main className='mainContainer'>
+            <h1>COSTA FRIA</h1>
+        </main>
+    );
+}
