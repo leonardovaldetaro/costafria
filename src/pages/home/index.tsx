3acdf598be4449544a522e733ef685185a07b418
Leonardo Valdetaro <leonardovaldetaro@gmail.com>
2024-08-31 11:50:58 +0100

    teste para imagens responsivas do topo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,15 @@
 import Styles from './Home.module.scss';
+import img_top_md from 'assets/costaFria_top_md.jpg';
+import img_top_sm from 'assets/costaFria_top_sm.jpg';
 
 export default function Home () {
     return (
         <section className={Styles.top}>
-            <h1 className={Styles.top__title}>costa fria</h1>
+            <img src={img_top_sm}
+                srcSet={`${img_top_sm} 390w, ${img_top_md} 1366w`}
+                sizes="(max-width: 600px) 390px, (max-width: 2000) 1366px, 100vw"
+            	alt="" 
+                className={Styles.top__img} />
         </section>
     );
 }
